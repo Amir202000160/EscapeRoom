187ed6e8733371ff039e0155c3da1020f8b79082
Amir <118963548+Amir202000160@users.noreply.github.com>
2025-11-22 06:37:52 -0800

    RedTXT
    Make sure the Player Enters his name

--- Assets/Scripts/SavePlayerData.cs
+++ Assets/Scripts/SavePlayerData.cs
@@ -6,24 +6,37 @@ using UnityEngine.UI;
 public class SavePlayerData : MonoBehaviour
 {
    [SerializeField] public TMP_InputField playerName;
+    public GameObject txt;
     void Start()
     {
+        txt.SetActive(false);
 
-        
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
 
     }
 
     public void SaveDataAndLoadSceneB()
     {
-        string playerNamee = playerName.text;
-        PlayerPrefs.SetString("PLayerName",playerNamee);
-        PlayerPrefs.Save();
-        SceneManager.LoadScene(1);
+        if (string.IsNullOrEmpty(playerName.text))
+
+        {
+            txt.SetActive(true);
+        }
+        else
+        {
+            string playerNamee = playerName.text;
+            PlayerPrefs.SetString("PLayerName", playerNamee);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene(1);
+        }
+           
     }
+
 }
